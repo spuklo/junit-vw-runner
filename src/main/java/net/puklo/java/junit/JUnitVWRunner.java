f8325ea0e2eec6b00b823cbe51635bccb56cb795
Slawek Puklo <piszdomniejeszcze@gmail.com>
2015-10-12 18:09:58 +0100

    Added logic to detect CI servers.

--- src/main/java/net/puklo/java/junit/JUnitVWRunner.java
+++ src/main/java/net/puklo/java/junit/JUnitVWRunner.java
@@ -10,7 +10,23 @@ import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.Statement;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
 public class JUnitVWRunner extends BlockJUnit4ClassRunner {
+
+    private static final List<String> environmentVariables = Collections.unmodifiableList(
+            Arrays.asList(
+                    "CI", "CONTINUOUS_INTEGRATION", // generic env variables, works for Travis
+                    "JENKINS_URL", // Jenkins
+                    "HUDSON_URL", // Hudson (and Jenkins)
+                    "bamboo_planKey", // Bamboo
+                    "TF_BUILD", // MS TFS
+                    "TEAMCITY_VERSION", // TeamCity
+                    "BUILDKITE" // Buildkite
+            ));
 
     public JUnitVWRunner(final Class<?> klass) throws InitializationError {
         super(klass);
@@ -20,10 +36,10 @@ public class JUnitVWRunner extends BlockJUnit4ClassRunner {
     protected void runChild(final FrameworkMethod method,
                             final RunNotifier notifier) {
         final Description description = describeChild(method);
-        if (this.isIgnored(method)) {
+        if (isIgnored(method)) {
             notifier.fireTestIgnored(description);
         } else {
-            runVwLeaf(super.methodBlock(method), description, notifier);
+            runCurrentLeaf(methodBlock(method), description, notifier);
         }
     }
 
@@ -32,9 +48,9 @@ public class JUnitVWRunner extends BlockJUnit4ClassRunner {
         return child.getAnnotation(Ignore.class) != null;
     }
 
-    protected final void runVwLeaf(final Statement statement,
-                                   final Description description,
-                                   final RunNotifier notifier) {
+    protected final void runCurrentLeaf(final Statement statement,
+                                        final Description description,
+                                        final RunNotifier notifier) {
         final EachTestNotifier eachNotifier = new EachTestNotifier(notifier, description);
         eachNotifier.fireTestStarted();
         try {
@@ -42,10 +58,29 @@ public class JUnitVWRunner extends BlockJUnit4ClassRunner {
         } catch (final AssumptionViolatedException e) {
             eachNotifier.addFailedAssumption(e);
         } catch (final Throwable e) {
-            // no no no...
+            handle(eachNotifier, e);
         } finally {
             eachNotifier.fireTestFinished();
         }
     }
+
+    private void handle(final EachTestNotifier eachNotifier,
+                        final Throwable throwable) {
+        final boolean iShouldActuallyCare = isItFriendlyEnvironment();
+        if (iShouldActuallyCare) {
+            eachNotifier.addFailure(throwable);
+        }
+    }
+
+    private boolean isItFriendlyEnvironment() {
+        final Properties currentSystemProperties = System.getProperties();
+        for (final String envVar : environmentVariables) {
+            if (currentSystemProperties.containsKey(envVar)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
--- src/test/java/net/puklo/java/junit/SampleUsage.java
+++ src/test/java/net/puklo/java/junit/SampleUsage.java
@@ -6,15 +6,15 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnitVWRunner.class)
-public class ExampleTest {
+public class SampleUsage {
 
     @Test
-    public void should_pass() {
+    public void should_pass_only_in_friendly_environment() {
         assertTrue(1 == 2);
     }
 
     @Test
-    public void should_pass_as_you_would_hope() {
+    public void should_pass_regardless_of_environment() {
         assertTrue(2 == 2);
     }
 }
