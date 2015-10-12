821dbba8900fb9d9eb1097409e3298526bc60d1e
Slawek Puklo <piszdomniejeszcze@gmail.com>
2015-10-12 07:33:28 +0100

    Initial commit

---
+++ src/main/java/net/puklo/java/junit/JUnitVWRunner.java
@@ -0,0 +1,45 @@
+package net.puklo.java.junit;
+
+import org.junit.internal.AssumptionViolatedException;
+import org.junit.internal.runners.model.EachTestNotifier;
+import org.junit.runner.Description;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.Statement;
+
+public class JUnitVWRunner extends BlockJUnit4ClassRunner {
+
+    public JUnitVWRunner(final Class<?> klass) throws InitializationError {
+        super(klass);
+    }
+
+    @Override
+    protected void runChild(final FrameworkMethod method,
+                            final RunNotifier notifier) {
+        final Description description = describeChild(method);
+        if (isIgnored(method)) {
+            notifier.fireTestIgnored(description);
+        } else {
+            runVwLeaf(methodBlock(method), description, notifier);
+        }
+    }
+
+    protected final void runVwLeaf(final Statement statement,
+                                   final Description description,
+                                   final RunNotifier notifier) {
+        final EachTestNotifier eachNotifier = new EachTestNotifier(notifier, description);
+        eachNotifier.fireTestStarted();
+        try {
+            statement.evaluate();
+        } catch (final AssumptionViolatedException e) {
+            eachNotifier.addFailedAssumption(e);
+        } catch (final Throwable e) {
+            // no no no...
+        } finally {
+            eachNotifier.fireTestFinished();
+        }
+    }
+
+}
---
+++ src/test/java/net/puklo/java/junit/JUnitVWRunnerTest.java
@@ -0,0 +1,14 @@
+package net.puklo.java.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JUnitVWRunnerTest {
+
+    @Test // why bother....
+    public void should_pass_test() {
+        assertTrue(true);
+    }
+
+}
